807b6caa01641e4a6e016b1260afc49e2ed96827
belamide231 <132540440+belamide231@users.noreply.github.com>
2025-02-19 00:48:20 +0800

    .

--- back/src/controllers/widgetController.ts
+++ back/src/controllers/widgetController.ts
@@ -5,7 +5,12 @@ export const widgetController = Router();
 widgetController
 .use(static_(path.join(__dirname, '../../public/widget/browser')));
 
+const validDomains = [
+    'http://localhost:3000',
+    'http://www.ibcauto.com'
+];
+
 widgetController
-.get('/widget/chat', (req: Request, res: Response) => {
-    return res.sendFile(path.join(__dirname, '../../public/widget/browser/index.html'));
+.get('/widget/chat', (req: Request, res: Response): any => {
+    return validDomains.some(x => req.headers.referer?.includes(x)) ? res.sendFile(path.join(__dirname, '../../public/widget/browser/index.html')) : res.sendStatus(401);
 })
